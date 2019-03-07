c9b2da56225e2434b2b2955c798a94aa9c3d3f2c
Yoshiki YAMADA <nyamadandan@gmail.com>
2019-03-07 23:31:38 +0900

    Slice

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,5 @@
 fn main() {
-    let _x = true;
-    let _y: bool = false;
-
-    let _x = 'x';
-    let _two_hearts: char = '💕';
-
-    let _x: i32 = 42;
-    let _y: f64 = 1.0;
-
-    let _a = [1, 2, 3];
-    let mut _m = [1, 2, 3];
-
-    let a = [0; 20];
-
-    println!("a has {} elements", a.len());
-
-    let names = ["Graydon", "Brian", "Niko"];
-
-    println!("The second name is: {}", names[1]);
+    let a = [0, 1, 2, 3, 4];
+    let complete = &a[..];
+    let middle = &a[1..4];
 }
