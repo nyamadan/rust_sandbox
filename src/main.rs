189d47c6241ca4d1c2ed0934f35346bf1ae2cbe5
Yoshiki YAMADA <nyamadandan@gmail.com>
2019-03-07 23:40:04 +0900

    tuple

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
 fn main() {
-    let a = [0, 1, 2, 3, 4];
-    let complete = &a[..];
-    let middle = &a[1..4];
+    let mut x = (1, 2);
+    let y = (2, 3);
+
+    x = y;
+
+    let tuple = (1, 2, 3);
+    let (x, y, z) = tuple;
+
+    let t0 = tuple.0;
+    let t1 = tuple.1;
+    let t2 = tuple.2;
+    println!("x: {}", t2);
+
+    fn foo(x: i32) -> i32 {
+        x
+    }
+
+    let x: fn(i32) -> i32 = foo;
 }
