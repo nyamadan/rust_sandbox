4b65bc9568fa929813dafa9a182a597fc264e0a9
Yoshiki YAMADA <nyamadandan@gmail.com>
2019-03-08 23:33:23 +0900

    if

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,7 @@
 fn main() {
-    let mut x = (1, 2);
-    let y = (2, 3);
+    let x = 5;
 
-    x = y;
+    let y: i32 = if x == 5 { 10 } else { 15 };
 
-    let tuple = (1, 2, 3);
-    let (x, y, z) = tuple;
-
-    let t0 = tuple.0;
-    let t1 = tuple.1;
-    let t2 = tuple.2;
-    println!("x: {}", t2);
-
-    fn foo(x: i32) -> i32 {
-        x
-    }
-
-    let x: fn(i32) -> i32 = foo;
+    println!("y is {}", y)
 }
