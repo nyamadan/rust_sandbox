d27d73fd4b1587ce51c85fec341db7ddece5754a
Yoshiki YAMADA <nyamadandan@gmail.com>
2019-03-09 21:10:59 +0900

    loop

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 fn main() {
-    loop {
-        println!("loop forever!");
+    let mut x = 5;
+    let mut done = false;
+
+    while !done {
+        x += x - 3;
+
+        println!("{}", x);
+
+        if x % 5 == 0 {
+            done = true;
+        }
+    }
+
+    for x in 0..19 {
+        println!("{}", x);
     }
 }
