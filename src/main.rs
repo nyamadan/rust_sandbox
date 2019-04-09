b5541f9f9409d7574a13def53ac5d8cc53261737
Yoshiki YAMADA <nyamadandan@gmail.com>
2019-04-09 22:47:23 +0900

    所有権の練習

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
+fn sum_len(v1: &Vec<i32>, v2: &Vec<i32>) -> usize {
+    v1.len() + v2.len()
+}
+
 fn main() {
-    'outer: for x in 0..10 {
-        'inner: for y in 0..10 {
-            if x % 2 == 0 { continue 'outer; }
-            if y % 2 == 0 { continue 'inner; }
-            println!("{}, {}", x, y);
-        }
-    }
+    let v1 = vec![1, 2, 3];
+    let v2 = vec![1, 2, 3];
+
+    let sum = sum_len(&v1, &v2);
+
+    println!("v1.len: {}", v1.len());
+    println!("v2.len: {}", v2.len());
+    println!("sum_len: {}", sum);
 }
