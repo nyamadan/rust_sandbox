45a48fd34f9a3e7e6c208c9b3ba9f44dcabfbb68
Yoshiki YAMADA <nyamadandan@gmail.com>
2019-04-10 22:39:27 +0900

    mut

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,9 @@
-fn sum_len(v1: &Vec<i32>, v2: &Vec<i32>) -> usize {
-    v1.len() + v2.len()
-}
-
 fn main() {
-    let v1 = vec![1, 2, 3];
-    let v2 = vec![1, 2, 3];
+    let mut x = 5;
+    {
+        let y = &mut x;
+        *y += 1;
+    }
 
-    let sum = sum_len(&v1, &v2);
-
-    println!("v1.len: {}", v1.len());
-    println!("v2.len: {}", v2.len());
-    println!("sum_len: {}", sum);
+    println!("{}", x);
 }
